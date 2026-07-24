04ef8264e911cdd434a5e7edc3f5086c2219d048
exerti <2214881062@qq.com>
2026-07-24 10:41:03 +0800

    commit

--- 2607/0724/review/滑动窗口/无重复的最长字串.kt
+++ 2607/0724/review/滑动窗口/无重复的最长字串.kt
@@ -1,5 +1,7 @@
 package review
 
+import kotlin.math.max
+
 /**
  * ⚠️ 上次失误:
  * 1. HashMap<String,Int> 键类型错误，s[right] 返回 Char 非 String
@@ -8,9 +10,23 @@ package review
  * 📈 掌握度: 2/5 | 累计错误: 3次 | 间隔: 3天 | 下次复习: 07-26
  */
 
+//preIndx >= left = "上次出现的位置在窗口左边界之右 → 还在窗口里 → 得收缩"。这是滑动窗口的核心判断。
 class LongestNoRepeatSubstring {
     fun lengthOfLongestSubstring(s: String): Int {
-        TODO()
+        val map = HashMap<Char, Int>()
+        var maxCount = 0
+        val size = s.length
+        var left = 0
+        for (right in 0..size - 1) {
+            val ch = s[right]
+            val preIndx = map[ch]
+            if (preIndx != null && preIndx >= left) {
+                left = preIndx + 1
+            }
+            map[ch] = right
+            maxCount = maxOf(maxCount, right - left + 1)
+        }
+        return maxCount
     }
 }
 
